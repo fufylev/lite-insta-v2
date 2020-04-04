33f00c3d5291d14c6d75746cc4c828c077e6958f
Andrey <35691656+fufylev@users.noreply.github.com>
2020-04-04 21:22:24 +0300

    Settings: eslint + babel Settings: eslint + babel

--- client/app/index.js
+++ client/app/index.js
@@ -1,14 +1,5 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                Hello world
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
