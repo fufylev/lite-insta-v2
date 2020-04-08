5d376f9ae036f0c6f89c05d4b0bb12fa66d6353c
Andrey <35691656+fufylev@users.noreply.github.com>
2020-04-08 16:27:17 +0300

    Project will be rewritten on the TypeScript
    Feat: Project from CLIENT folder will be rewritten on the TypeScript

---
+++ client_ts/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className='container text-center mt-5'>Project from CLIENT folder will be rewritten on the TypeScript</h2>
+    </div>
+  );
+}
+
+export default App;
