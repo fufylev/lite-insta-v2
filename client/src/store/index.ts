4b7670045e4b910ec78f9363dfb2f64effaad36c
Andrey <35691656+fufylev@users.noreply.github.com>
2020-04-12 14:13:43 +0300

    feat: redux persister was implemented
    feat: redux persister was implemented

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,8 +1,16 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore, Store } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { Persistor } from 'redux-persist/es/types';
 import { authReducer } from './auth/reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
 const rootReducer = combineReducers({
   auth: authReducer,
@@ -10,14 +18,18 @@ const rootReducer = combineReducers({
 
 export type AppState = ReturnType<typeof rootReducer>;
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export default function configureStore() {
   const middlewares = [thunkMiddleware, logger];
   const middleWareEnhancer = applyMiddleware(...middlewares);
 
   const store = createStore(
-    rootReducer,
+    persistedReducer,
     composeWithDevTools(middleWareEnhancer),
   );
+  // @ts-ignore
+  let persistor: Persistor = persistStore<Store>(store);
 
-  return store;
+  return { store, persistor };
 }
